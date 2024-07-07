29708d717769cffce78b64fd2a39e814e0eaec89
AboGhfh2 <155238940+AboGhfh2@users.noreply.github.com>
2024-07-07 18:11:52 +0300

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',  // إعداد لتصدير المشروع كملفات HTML ثابتة
+    trailingSlash: true,
+  };
+  
+  export default nextConfig;
+  
